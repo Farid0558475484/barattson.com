f863909e126b36cab62040d4c02e1372b888aee7
Farid0558475484 <faridmh@code.edu.az>
2023-05-16 12:33:05 +0400

    feat:finish about page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,43 @@
-// $(document).ready(function () {
-//   $(".owl-carousel").owlCarousel();
-// });
-// $(".owl-carousel").owlCarousel({
-//   //Basic Speeds
-//   slideSpeed: 200,
-//   paginationSpeed: 800,
+document.addEventListener("DOMContentLoaded", function () {
+  var missionTab = document.getElementById("nav-home-tab");
+  var profileTab = document.getElementById("nav-profile-tab");
+  var cobTab = document.getElementById("nav-contact-tab");
+  var qualityTab = document.getElementById("nav-quality-tab");
 
-//   //Autoplay
-//   autoPlay: false,
-//   goToFirst: true,
-//   goToFirstSpeed: 1000,
+  var missionContent = document.getElementById("nav-home");
+  var profileContent = document.getElementById("nav-profile");
+  var cobContent = document.getElementById("nav-contact");
+  var qualityContent = document.getElementById("nav-quality");
 
-//   // Navigation
-//   navigation: false,
-//   navigationText: ["prev", "next"],
-//   pagination: true,
-//   paginationNumbers: true,
+  function showTabContent(tab, content) {
+    missionTab.classList.remove("active");
+    profileTab.classList.remove("active");
+    cobTab.classList.remove("active");
+    qualityTab.classList.remove("active");
 
-//   // Responsive
-//   responsive: true,
-//   items: 5,
-//   itemsDesktop: [1199, 4],
-//   itemsDesktopSmall: [980, 3],
-//   itemsTablet: [768, 2],
-//   itemsMobile: [479, 1],
-// });
+    missionContent.style.display = "none";
+    profileContent.style.display = "none";
+    cobContent.style.display = "none";
+    qualityContent.style.display = "none";
 
-console.log("hello");
+    tab.classList.add("active");
+    content.style.display = "block";
+    console.log("hello");
+  }
+
+  missionTab.addEventListener("click", function () {
+    showTabContent(missionTab, missionContent);
+  });
+
+  profileTab.addEventListener("click", function () {
+    showTabContent(profileTab, profileContent);
+  });
+
+  cobTab.addEventListener("click", function () {
+    showTabContent(cobTab, cobContent);
+  });
+
+  qualityTab.addEventListener("click", function () {
+    showTabContent(qualityTab, qualityContent);
+  });
+});
